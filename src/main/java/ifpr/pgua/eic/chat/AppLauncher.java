56093cb1b2ec4dfa5330865c3807d494b6e1a2a5
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2022-12-06 10:25:41 -0300

    ajustando

---
+++ src/main/java/ifpr/pgua/eic/chat/AppLauncher.java
@@ -0,0 +1,8 @@
+package ifpr.pgua.eic.chat;
+
+public class AppLauncher {
+    
+    public static void main(String[] args){
+        App.main(args);
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module ifpr.pgua.eic.chat {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens ifpr.pgua.eic.chat.cliente to javafx.fxml;
-    exports ifpr.pgua.eic.chat;
-}
